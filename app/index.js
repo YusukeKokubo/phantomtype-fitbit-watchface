6eae17b5f19c9b9d00fa5299bdf4bef83fb60269
YusukeKokubo <yusuke.kokubo@gmail.com>
2019-01-20 11:30:28 +0900

    initial commit

---
+++ app/index.js
@@ -0,0 +1,50 @@
+import document from "document";
+
+import * as simpleActivity from "./simple/activity";
+import * as simpleClock from "./simple/clock";
+import * as simpleHRM from "./simple/hrm";
+
+let background = document.getElementById("background");
+let txtTime = document.getElementById("txtTime");
+let txtSeconds = document.getElementById("txtSeconds");
+let txtDate = document.getElementById("txtDate");
+let txtHRM = document.getElementById("txtHRM");
+let iconHRM = document.getElementById("iconHRM");
+let imgHRM = iconHRM.getElementById("icon");
+let statsCycle = document.getElementById("stats-cycle");
+let statsCycleItems = statsCycle.getElementsByClassName("cycle-item");
+
+/* --------- CLOCK ---------- */
+function clockCallback(data) {
+  txtTime.text = data.time;
+  txtDate.text = data.date;
+  txtSeconds.text = data.seconds;
+}
+simpleClock.initialize("seconds", "longDate", clockCallback);
+
+/* ------- ACTIVITY --------- */
+function activityCallback(data) {
+  statsCycleItems.forEach((item, index) => {
+    let img = item.firstChild;
+    let txt = img.nextSibling;
+    txt.text = data[Object.keys(data)[index]].pretty;
+    // Reposition the activity icon to the left of the variable length text
+    img.x = txt.getBBox().x - txt.parent.getBBox().x - img.width - 7;
+  });
+}
+simpleActivity.initialize("seconds", activityCallback);
+
+/* -------- HRM ------------- */
+function hrmCallback(data) {
+  txtHRM.text = `${data.bpm}`;
+  if (data.zone === "out-of-range") {
+    imgHRM.href = "images/heart_open.png";
+  } else {
+    imgHRM.href = "images/heart_solid.png";
+  }
+  if (data.bpm !== "--") {
+    iconHRM.animate("highlight");
+  }
+}
+simpleHRM.initialize(hrmCallback);
+
